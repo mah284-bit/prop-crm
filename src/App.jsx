4091f5a665e2fbaa4464a68ed5728b044dc50199
Abid Mirza <mah284@gmail.com>
2026-04-07 16:55:30 +0400

    Remove comment between array elements

--- src/App.jsx
+++ src/App.jsx
@@ -2452,7 +2452,6 @@ function ProjectsModule({ currentUser, showToast, crmContext="sales", preloadedP
                       </div>
                     </td>
                   </tr>,
-                  {/* Expanded row */}
                   {isExp&&(
                     <tr key={proj.id+"_exp"}>
                       <td colSpan={8} style={{padding:"0 12px 12px",background:"#F7F9FC",borderBottom:"2px solid #E2E8F0"}}>
